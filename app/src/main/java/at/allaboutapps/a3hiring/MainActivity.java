9351fde47d59f23b133cedb43aac702232ef1276
Markus Gursch <markus.gursch@allaboutapps.at>
2019-09-13 14:05:54 +0200

    switched to material compoments theme, upgraded gradle plugin

--- app/src/main/java/at/allaboutapps/a3hiring/MainActivity.java
+++ app/src/main/java/at/allaboutapps/a3hiring/MainActivity.java
@@ -1,10 +1,11 @@
 package at.allaboutapps.a3hiring;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
